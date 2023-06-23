3d3693bd6a55a5ed2c278b37f0a85285518c3a84
Vika <vika.mirna.1998@gmail.com>
2023-06-23 21:39:54 +0300

    reverse about

--- vika_2021/vika_2021/urls.py
+++ vika_2021/vika_2021/urls.py
@@ -19,6 +19,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", views.home),
+    path('admin/', admin.site.urls),
+    path('', views.about),
+    path('home/', views.home),
+    path('reverse/', views.reverse, name='reversed'),
 ]
--- vika_2021/vika_2021/views.py
+++ vika_2021/vika_2021/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+def about(request):
+    a = 5+6
+    return render(request,'about.html',{'gretting':a})
+
+def reverse(request):
+    user_text = request.GET['username']
+    reverse = user_text[::-1]
+    return render(request,'reverse.html',{'word':reverse})
+
 def home(request):
-    return render(request,'home.html')
+    return HttpResponse('This is my home')
